9630333c9614d0658c1350d986c5da0f98543ddc
ymfnia <ymfnia@921a7764-98f0-11dd-bd71-f92a316d2d60>
2010-10-29 09:34:00 +0000

    MAKE....

--- trunk/gaozj/boot_new/include/type.h
+++ trunk/gaozj/boot_new/include/type.h
@@ -8,5 +8,8 @@ typedef unsigned int   u_int;
 typedef unsigned long u_int64;
 
 typedef long int64;
+//#define fastcall __attribute__((regparm(3)))
+#define asmlinkage CPP_ASMLINKAGE __attribute__((regparm(0))) 
 
 #endif
+
